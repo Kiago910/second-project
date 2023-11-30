1c4aa5b3f6c2fe6539d9d224f63887534664061f
Kiago910 <markkiago16@gmail.com>
2023-11-30 12:31:58 +0300

    random number

---
+++ qn_2.py
@@ -0,0 +1,23 @@
+import random
+number = random.randint(-10000, 10000)
+i = number
+print(abs(i))
+char = str(i)
+m=(len(char))
+letter = m-1
+last_digit=(char[letter])
+if int(last_digit)>5:
+    x=(f"last digit of {i} is")
+    y=("{} and is greater than 5".format(last_digit))
+    last=x+" "+y
+    print(last)
+if int(last_digit)==0:
+     x=(f"last digit of {i} is")
+     y=("{} and is zero".format(last_digit))
+     z=x+" "+y
+     print(z)
+if int(last_digit)<6 and int(last_digit)!=0:
+     x=(f"last digit of {i} is")
+     y=("{} and is less than 6 and not 0".format(last_digit))
+     less=x+" "+y
+     print(less)
