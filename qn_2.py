aa3613ab41c1d0147e074f86535d00ea68800e7f
Kiago910 <markkiago16@gmail.com>
2023-12-01 09:40:02 +0300

    range of numbers

--- qn_2.py
+++ qn_2.py
@@ -1,7 +1,6 @@
 import random
 number = random.randint(-10000, 10000)
 i = number
-print(abs(i))
 char = str(i)
 m=(len(char))
 letter = m-1
---
+++ qn_5.py
@@ -0,0 +1,2 @@
+for x in range(0,99):
+    print(f"{x:02d},"," ",end='')
