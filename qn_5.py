6d0bbe01e199a58fb200c05296b6d2fe5a2c6060
Kiago910 <markkiago16@gmail.com>
2023-12-01 09:44:15 +0300

    range of numbers

--- qn_5.py
+++ qn_5.py
@@ -1,2 +1,4 @@
+# end='' function is used to print different fnctions in a singlr linr
+#when using f string  the :02d is used to specify the number of digits
 for x in range(0,99):
     print(f"{x:02d},"," ",end='')
